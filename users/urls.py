8cf0750c0020c067f0b2ddd5a61d5f8a061c4fdd
Isaac Woodruff <isaacjahwoodruff@gmail.com>
2022-09-01 13:19:43 +0100

    Updated views, links and max-width of sign in form

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='login'),
+    path('', views.sign_in, name='sign_in'),
+    path('sign-up', views.sign_up, name='sign_up'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
-def login(request):
+def sign_in(request):
     context = {
-        "page_title": "Login",
+        "page_title": "Sign In",
     }
-    return render(request, 'users/login-signup.html', context)
+    return render(request, 'users/sign-in.html', context)
+
+def sign_up(request):
+    context = {
+        "page_title": "Sign Up",
+    }
+    return render(request, 'users/sign-up.html', context)
