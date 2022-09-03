d5a7b0a35c3642261cc27c67b162d58c57148b96
Isaac Woodruff <isaacjahwoodruff@gmail.com>
2022-09-03 15:43:52 +0100

    Create Profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    '''
+    The Profile model extends the user model with a one-to-one relationship.
+    If the Profile is deleted then it automatically deletes the user too
+    '''
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    github = models.CharField(max_length=100, blank=True)
+    job_title = models.CharField(max_length=30, blank=True)
+    avatar = models.ImageField(upload_to='img')
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
