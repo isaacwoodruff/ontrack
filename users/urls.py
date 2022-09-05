fccf8dbaa1f927353c5381ff646988bb77901ca6
Isaac Woodruff <isaacjahwoodruff@gmail.com>
2022-09-05 17:22:41 +0100

    Created LoginView, added temp profile view

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
+from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
+from .forms import SignIn
 
 from . import views
 
 urlpatterns = [
-    path('', views.sign_in, name='sign_in'),
+   path('', LoginView.as_view(
+            extra_context={
+                'page_title':'Sign In'
+            },
+            authentication_form=SignIn,
+            template_name='users/sign-in.html',
+            redirect_authenticated_user=True),
+        name='sign_in'),
     path('sign-up', views.SignUpView.as_view(), name='sign_up'),
+    path('profile/', login_required(TemplateView.as_view(template_name="users/profile.html"))),
 ]
