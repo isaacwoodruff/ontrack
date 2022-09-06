3eae18f886805e98e9fd90dc5eae6779a5c1c0cb
Isaac Woodruff <isaacjahwoodruff@gmail.com>
2022-09-06 10:17:42 +0100

    Added tests for Profile Model

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from users.models import Profile
+
+class ProfileModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create(username='elliotanderson', password='password')
+
+    def test_github(self):
+        user = User.objects.get(username='elliotanderson')
+        user.profile.github = "github.com/elliotanderson"
+        self.assertEqual(user.profile.github, "github.com/elliotanderson")
+
+    def test_job_title(self):
+        user = User.objects.get(username='elliotanderson')
+        user.profile.job_title = "Chaos Engineer"
+        self.assertEqual(user.profile.job_title, "Chaos Engineer")
