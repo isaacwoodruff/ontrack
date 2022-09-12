c2d4126ca7abf457776f20a5db5df29cac25d970
Isaac Woodruff <isaacjahwoodruff@gmail.com>
2022-09-12 17:03:06 +0100

    Fixed if statement always True in database url

--- ontrack/settings.py
+++ ontrack/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'ontrack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-if os.environ.get('DEVELOPMENT'):
+if os.environ.get('DEVELOPMENT') == True:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
