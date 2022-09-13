99b7177ed728d5427d2d9c50f29e56c831d5a62d
Isaac Woodruff <isaacjahwoodruff@gmail.com>
2022-09-13 07:46:17 +0100

    Refactored database url code

--- ontrack/settings.py
+++ ontrack/settings.py
@@ -86,17 +86,16 @@ WSGI_APPLICATION = 'ontrack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-if os.environ.get('DEVELOPMENT') == 'True':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
+}
+
+if os.environ.get('DEVELOPMENT') != 'True':
+    DATABASES['default'] = dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600)
 
 
 # Password validation
